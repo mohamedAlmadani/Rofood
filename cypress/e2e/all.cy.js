7d1b7e35ce18aa50fd89e0ee613a7aa789806291
mohammed Almadani <mzalmadany@gmail.com>
2023-01-07 10:41:19 +0200

    first version

--- cypress/e2e/all.cy.js
+++ cypress/e2e/all.cy.js
@@ -1,9 +1,9 @@
 
 import './glassesSales/salesNewInvoice.cy.js';
-//import './glassesSales/Inventory.cy.js';
-//import './glassesSales/outstandingrAndeplacementAndRefundInvoice.cy.js';
-//import './glassesSales/Purchases.cy.js';
-//import './glassesSales/cashier.cy.js';
+import './glassesSales/Inventory.cy.js';
+import './glassesSales/outstandingrAndeplacementAndRefundInvoice.cy.js';
+import './glassesSales/Purchases.cy.js';
+import './glassesSales/cashier.cy.js';
 
 
 
