ead30101b5e7b4156b1b6429000a61a5a99d4a7f
mohammed Almadani <mzalmadany@gmail.com>
2023-01-07 15:07:13 +0200

    fix elelment

--- cypress/e2e/all.cy.js
+++ cypress/e2e/all.cy.js
@@ -1,15 +1,9 @@
 
-import './glassesSales/salesNewInvoice.cy.js';
+//import './glassesSales/salesNewInvoice.cy.js';
 //import './glassesSales/Inventory.cy.js';
 //import './glassesSales/outstandingrAndeplacementAndRefundInvoice.cy.js';
 //import './glassesSales/Purchases.cy.js';
-//import './glassesSales/cashier.cy.js';
+import './glassesSales/cashier.cy.js';
 
 
-/*
-post{
-    always{
-        publishHTML([allowmissing:false,alwaysLinkToLastBuild:false,keepAll:true,reportDir: 'cypress/report',reportFiles: 'index.html',reportName: 'HTML Report',reportTitles:''])
-    }
-}
-*/
+
