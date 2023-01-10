10669920d5dae01918f6c68244cf08ee07ef95ec
mohammed Almadani <mzalmadany@gmail.com>
2023-01-10 04:27:05 +0200

    all file for run

--- Jenkinsfile
+++ Jenkinsfile
@@ -29,9 +29,7 @@ pipeline{
         
        }
     }
-    post {
-       
-}
+ 
    
 
 }
--- cypress/e2e/all.cy.js
+++ cypress/e2e/all.cy.js
@@ -1,8 +1,8 @@
 
-//import './glassesSales/salesNewInvoice.cy.js';
-//import './glassesSales/Inventory.cy.js';
-//import './glassesSales/outstandingrAndeplacementAndRefundInvoice.cy.js';
-//import './glassesSales/Purchases.cy.js';
+import './glassesSales/salesNewInvoice.cy.js';
+import './glassesSales/Inventory.cy.js';
+import './glassesSales/outstandingrAndeplacementAndRefundInvoice.cy.js';
+import './glassesSales/Purchases.cy.js';
 import './glassesSales/cashier.cy.js';
 
 
