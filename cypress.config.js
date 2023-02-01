e6d843dfec85c650101d9d0fc0eaaf17e6311c37
mohammed Almadani <mzalmadany@gmail.com>
2023-02-01 15:56:21 +0200

    add schedule

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,8 @@ module.exports = defineConfig({
     viewportWidth: 1440,
     viewportHeight: 900,
     specPattern:'cypress/e2e/all.cy.js',
+    experimentalWebKitSupport: true,
+    videoUploadOnPasses: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
