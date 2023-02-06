aa7ed6ce1a7f60a91ffb32f5859d013f132eebea
mohammed Almadani <mzalmadany@gmail.com>
2023-02-06 07:07:31 +0200

    v10

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
     viewportHeight: 900,
     specPattern:'cypress/e2e/all.cy.js',
     experimentalWebKitSupport: true,
-    videoUploadOnPasses: true,
+    videoUploadOnPasses: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
